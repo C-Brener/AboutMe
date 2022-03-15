051de4a3ea9201d5de0f4183feb5c59db9dac5b6
brenermr <brenercaique0806@gmail.com>
2022-03-14 22:40:33 -0300

    AboutMe

--- app/src/main/java/com/ciandt/aboutme/MainActivity.kt
+++ app/src/main/java/com/ciandt/aboutme/MainActivity.kt
@@ -2,6 +2,8 @@ package com.ciandt.aboutme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.ciandt.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +14,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.bioContentScroll.isVerticalScrollBarEnabled=false
+
+        binding.doneButton.setOnClickListener {
+            val name = binding.nicknameInput.text.toString()
+            if (name.isBlank()){
+                Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
+            }else{
+                binding.textNickname.text = name
+                binding.textNickname.visibility = View.VISIBLE
+            }
+
+        }
     }
 }
