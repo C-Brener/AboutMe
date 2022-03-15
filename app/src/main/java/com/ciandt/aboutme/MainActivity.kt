678b2326b97554e2a017bab04c2b1a90e95a6a3e
brenermr <brenercaique0806@gmail.com>
2022-03-14 22:49:27 -0300

    AboutMe

--- app/src/main/java/com/ciandt/aboutme/MainActivity.kt
+++ app/src/main/java/com/ciandt/aboutme/MainActivity.kt
@@ -7,21 +7,26 @@ import android.widget.Toast
 import com.ciandt.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding:ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.bioContentScroll.isVerticalScrollBarEnabled=false
+        binding.bioContentScroll.isVerticalScrollBarEnabled = false
 
         binding.doneButton.setOnClickListener {
             val name = binding.nicknameInput.text.toString()
-            if (name.isBlank()){
+            if (name.isBlank()) {
                 Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
-            }else{
-                binding.textNickname.text = name
-                binding.textNickname.visibility = View.VISIBLE
+            } else {
+                with(binding) {
+                    textNickname.text = name
+                    textNickname.visibility = View.VISIBLE
+                    doneButton.visibility = View.GONE
+                    nicknameInput.visibility = View.GONE
+                }
+
             }
 
         }
